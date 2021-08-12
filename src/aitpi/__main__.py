536ae2fe9b9673748aa9c9f78abb459ade361022
Caden Churchman <ccaden21@gmail.com>
2021-08-12 09:43:29 -0500

    Made registry non-static

--- src/aitpi/__main__.py
+++ src/aitpi/__main__.py
@@ -1,1 +1,17 @@
-import aitpi.command_registry
+from aitpi.aitpi import Aitpi
+from aitpi.printer import Printer
+from aitpi.postal_service import PostalService
+
+class Watcher():
+    def consume(self, message):
+        Printer.print(" Watcher: %s %s" % (message.data, message.event))
+
+PostalService.addConsumer([1,2,3,4,5,6,7,8,9], PostalService.GLOBAL_SUBSCRIPTION, Watcher())
+
+Aitpi.addRegistry("../example_command_registry.json")
+Aitpi.addRegistry("../reg2.json")
+
+Aitpi.initInput("../example_input.json")
+
+while (True):
+    input()
--- src/aitpi/command_registry.py
+++ src/aitpi/command_registry.py
@@ -9,24 +9,39 @@ import time
 from aitpi.printer import Printer
 
 class CommandRegistry():
-    """Represents a 'registry' of all commands that the user can execute
+    """ Represents a 'registry' of all commands that the user can execute
     """
+    
+    _registries = []
 
     def __init__(self, commandRegJson=None, foldersJson=None):
-        """Setup data structures
+        """ Setup data structures
         """
         self._commands = None
         self._foldersForCommands = None
         self._folderCommands = {}
         self._commands = MirroredJson(commandRegJson)
-        self._foldersForCommands = MirroredJson(foldersJson)
+        self._foldersForCommands = MirroredJson(foldersJson) if foldersJson != None else None
         PostalService.addConsumer(
-            [CommandRegistryCommand.msgId, FolderMessage.msgId],
+            [CommandRegistryCommand.msgId],
             PostalService.GLOBAL_SUBSCRIPTION,
             self
         )
+        if (self._foldersForCommands != None):
+            self.initFoldersForCommands()
+        CommandRegistry._registries.append(self)
 
-        self.reloadFolders()
+
+    def initFoldersForCommands(self):
+        """ Takes care of initalizing the foldersForCommands setup
+        """
+
+        # Now we need to subscribe to the folder messages
+        PostalService.addConsumer(
+            [FolderMessage.msgId],
+            PostalService.GLOBAL_SUBSCRIPTION,
+            self
+        )
         for folder in self._foldersForCommands._settings:
             if (not isdir(folder['path'])):
                 Printer.print("Did not find dir '{}' creating...".format(folder['path']))
@@ -43,47 +58,59 @@ class CommandRegistry():
                 Printer.print("Invalid folder message id '%s'" % folder['id'], Printer.ERROR)
             # Add watch to every folder
 
-    def contains(self, command):
-        for commandList in self._commands.keys():
-            if (command in self._commands[commandList].keys()):
-                return True
+    @staticmethod
+    def contains(command):
+        """ Returns whether the command exists in this registry
+        """
+        for registry in CommandRegistry._registries:
+            for commandList in registry._commands.keys():
+                if (command in registry._commands[commandList].keys()):
+                    return True
         return False
 
-    def reloadFolders(self):
-        """Reloads all the command folders
+    def findByProperty(self, array, propertyName, propertyVal):
+        """ Find an item in an array by a property of that item
+
+            Returns:
+                bool: if contains
         """
+        for item in array:
+            if (item[propertyName] == propertyVal):
+                return item
+        return None
+
+    def reloadFolder(self, folder):
+        """ Reloads all the command folders
+        """
+        item = self.findByProperty(self._foldersForCommands, 'path', folder)
+        if (item == None):
+            return
         # Clear out all old commands
-        for T in self._folderCommands.keys():
-            for command in self._folderCommands[T].keys():
-                if (command in self._commands[T].keys()):
-                    self._commands[T].pop(command)
-        # Reset what we have so far.
-        self._folderCommands = {}
-        for index in range(0, len(self._foldersForCommands._settings)):
-            for root, dirs, files in os.walk(
-                self._foldersForCommands[index]['path'],
-                topdown=False
-                ):
-                for name in files:
-                    msgId = self._foldersForCommands[index]["id"]
-                    T = self._foldersForCommands[index]["type"]
-                    if (not T in self._folderCommands.keys()):
-                        self._folderCommands[T] = {}
-                    self._folderCommands[T][name] = {}
-                    self._folderCommands[T][name]['id'] = msgId
-                    self._folderCommands[T][name]['mechanism'] = self._foldersForCommands[index]["mechanism"]
-        # Install each command back into the library
-        for T in self._folderCommands.keys():
-            for command in self._folderCommands[T].keys():
+        # We assume the folder has changed entirely
+        if (item['type'] in self._commands.keys()):
+            for command in self._commands[item['type']]:
+                if (command['path'] == folder):
+                    self._commands[item['type']].pop(command)
+        
+        # Add all the files to the registry
+        for root, dirs, files in os.walk(
+            folder,
+            topdown=False
+            ):
+            for name in files:
+                msgId = item['id']
+                T = item['type']
                 if (not T in self._commands.keys()):
                     self._commands[T] = {}
-                print(command, self._folderCommands[T][command])
-                self._commands[T][command] = self._folderCommands[T][command]
-                print()
+                self._commands[T][name] = {}
+                self._commands[T][name]['id'] = msgId
+                self._commands[T][name]['mechanism'] = item['mechanism']
+        
+        # Update the mirrored json
         self.save()
 
     def getAllCommands(self):
-        """Gets the list of commands
+        """ Gets the list of commands
 
         Returns:
             list: commands
@@ -95,7 +122,7 @@ class CommandRegistry():
         return ret
 
     def getCommands(self, T):
-        """Gets a dict of commands by type
+        """ Gets a dict of commands by type
 
         Returns:
             Dictionary: commands
@@ -109,7 +136,7 @@ class CommandRegistry():
         return self._commands.keys()
 
     def addCommand(self, name, messageID, T, mechanism):
-        """Adds a command to the library
+        """ Adds a command to the library
 
         Args:
             name (str): The name of the command
@@ -128,9 +155,8 @@ class CommandRegistry():
         self.save()
         return True
 
-    @staticmethod
-    def removeCommand(T, name):
-        """Removes a command
+    def removeCommand(self, T, name):
+        """ Removes a command
 
         Args:
             name (str): The name to remove
@@ -138,28 +164,25 @@ class CommandRegistry():
         self._commands[T].pop(name)
         self.save()
 
-    @staticmethod
-    def save():
-        """Saves all the commands
+    def save(self):
+        """ Saves all the commands to the mirrored json
         """
         self._commands.save()
 
-    @staticmethod
-    def consume(msg):
-        """Handles sending actuall commands,
-           and watches folder commands for changes.
+    def consume(self, msg):
+        """ Handles sending actuall commands,
+            and watches folder commands for changes.
 
         Args:
             msg (Message): Either a command, or a folder update
         """
-        if (msg.msgId == selfCommand.msgId):
+        if (msg.msgId == CommandRegistryCommand.msgId):
             self.send(msg)
         elif (msg.msgId == FolderMessage.msgId):
-            self.reloadFolders()
+            self.reloadFolder(msg.data)
 
-    @staticmethod
-    def send(msg):
-        """Handles sending a command to where the library says
+    def send(self, msg):
+        """ Handles sending a command to where the library says
 
         Args:
             command (unknown): Some data that will be sent
@@ -172,4 +195,3 @@ class CommandRegistry():
                 msg.msgId = int(self._commands[T][command]['id'])
                 PostalService.sendMessage(msg)
                 return
-        Printer.print("'{}' not found in the command library".format(command))
